bddf4339e0cd13d9200d3232b2cfc79f803ec298
elhmn <elhmn@localhost.localdomain>
2018-02-04 04:13:08 +0100

    movement first part

--- srcs/js/game.js
+++ srcs/js/game.js
@@ -2,28 +2,35 @@ var entities = [[]];
 
 var player = null;
 
-var level = 
-[
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0, 0]
-    [0, 0, 0, 0, 0, 0, 0, 0]
-];
+var level1 = 
+{
+   map : [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ],
+
+    partCount : 0
+};
 
 
 var EntityType = 
 {
-    BLOCK : 0,
-    HERO : 1,
-    WATER : 2,
-    FIRE : 3,
-    EARTH : 4,
-    AIR : 5,
-    BLACKHOLE : 6
+    EMPTY : 0,
+    BLOCK : 1,
+    HERO : 2,
+    WATER : 3,
+    FIRE : 4,
+    EARTH : 5,
+    AIR : 6,
+    BLACKHOLE : 7
 };
 
 //Set object as constant
@@ -35,11 +42,17 @@ var Hero = function ()
     this.speed = 1;
     this.speedMax = 4;
     this.isPullDown = false;
+    this.isAlive = true;
+    this.canEndLevel = false;
+    this.collected = 0;
+    this.isOnBlock = false;
+    this.isOnAirInfluence = false;
 
     this.move = function (keyValue)
     {
-        var x = player.pos.x;
-        var y = player.pos.y;
+        //Implement movements
+        var old_x = x = player.pos.x;
+        var old_y = y = player.pos.y;
         switch (keyValue)
         {
             case "ArrowRight":
@@ -59,18 +72,85 @@ var Hero = function ()
                 break;
 
             default:
-            console.log("key unhandled");
+                console.log("key unhandled");
             break;
         }
-//        if (x && y)
-  //      {
-//
-    //    }
-  //      if ()
-  //      {
-//
-  //      }
-   //     entities[][] = ;
+
+        //Check map limits
+        var len = entities[0].length;
+        if (x >= len)
+            x = len - 1;
+        else if (x > 0)
+            x = 0;
+        len = entities.length;
+        if (y >= len)
+            y = len - 1;
+        else if ( y < 0)
+            y = 0;
+
+        switch (entities[y][x])
+        {
+            case EntityType.BLOCK:
+                move(keyValue);
+                break;
+
+            case EntityType.EMPTY:
+                if (!isOnBlock && !isOnAirInfluence)
+                {
+                    var tmp = Entities[y][x];
+                    Entities[y][x] = player;
+                    Entities[old_y][old_x] = tmp;
+                }
+                break;
+
+            case EntityType.BLACKHOLE:
+                if (!isOnBlock && !isOnAirInfluence)
+                {
+                    this.isAlive = false;
+                    player.partCount++;
+                }
+                break;
+
+            case EntityType.AIR:
+                if (isOnBlock)
+                {
+                    isOnBlock = false;
+                }
+                isOnAirInfluence = true;
+                console.log("AIR on ", x, ", ", y);
+                move(keyValue);
+                player.partCount++;
+                break;
+
+            case EntityType.EARTH:
+                if (isOnBlock)
+                {
+                    isOnBlock = false;
+                }
+                if (isOnAirInfluence)
+                {
+                    isOnAirInfluence = false;
+                }
+                player.isPullDown = true;
+                player.partCount++;
+                break;
+
+            case EntityType.WATER:
+                player.speed--;
+                player.partCount++;
+                break;
+
+            case EntityType.FIRE:
+                player.speed++;
+                player.partCount++;
+                break;
+        }
+
+        //On Air Element influence
+        if (isOnAirInfluence)
+        {
+            move(keyValue);
+        }
     };
 
     console.log("Hero object created");
@@ -226,6 +306,22 @@ ShowEntityList = function ()
 var canvas = null;
 var ctx = null;
 
+var check_game_state = function ()
+{
+    if (!player.isAlive)
+    {
+        //reset level;
+        console.log("reset level");
+        player.isAlive = true;
+    }
+
+    if (player.collected == level.partCount)
+    {
+        player.canEndLevel = true;
+        //
+    }
+}
+
 var keyHandler = function (event)
 {
     var code = event.key;
@@ -246,8 +342,14 @@ var keyHandler = function (event)
 
         default:
             console.log("key unhandled");
-            break;
+            return ;
+    
     }
+
+    //
+    player.move(code);
+//    check_game_state();
+//    draw();
 }
 
 var run_game = function ()
